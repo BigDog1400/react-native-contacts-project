c19e5dc816ca8ccc87515f1ae4ade9142ae8599d
Isaac Peña <isaac.pr99@gmail.com>
2021-06-28 01:49:47 -0400

    feat: create form for sign up

--- src/components/Signup/index.tsx
+++ src/components/Signup/index.tsx
@@ -1,14 +1,27 @@
 import {useNavigation} from '@react-navigation/core';
-import React, {useState} from 'react';
+import React from 'react';
 import {Image, Text, View} from 'react-native';
 import {TouchableOpacity} from 'react-native-gesture-handler';
 import {Container} from '../../components/common/Container';
 import {CustomButton} from '../../components/common/CustomButton';
 import {Input} from '../../components/common/Input';
 import {LOGIN} from '../../constants/routeNames';
+import {userSignUpFormValues} from '../../types/userSignUpFormValues';
 import styles from './styles';
-const RegisterComponent = () => {
-  const [text, onChangeText] = useState('Useless Text');
+
+type RegisterComponentProps = {
+  onSubmit: () => void;
+  form: userSignUpFormValues;
+  errors: userSignUpFormValues;
+  onChange: ({name, value}: {name: string; value: string}) => void;
+};
+
+const RegisterComponent = ({
+  onSubmit,
+  form,
+  errors,
+  onChange,
+}: RegisterComponentProps) => {
   const {navigate} = useNavigation();
   return (
     <Container>
@@ -21,45 +34,56 @@ const RegisterComponent = () => {
         <Text style={styles.subTitle}>Create a Free Account</Text>
         <View style={styles.form}>
           <Input
+            label="Username"
+            value={form.userName}
+            onChangeText={value => {
+              onChange({name: 'userName', value});
+            }}
+            placeholder="Enter Username"
+            error={errors.userName}
+          />
+          <Input
             label="First Name"
-            value={text}
-            onChangeText={text => onChangeText(text)}
+            value={form.firstName}
             placeholder="Enter First Name"
+            onChangeText={value => {
+              onChange({name: 'firstName', value});
+            }}
+            error={errors.firstName}
           />
           <Input
             label="Last Name"
-            value={text}
-            onChangeText={text => onChangeText(text)}
+            value={form.lastName}
+            onChangeText={value => {
+              onChange({name: 'lastName', value});
+            }}
             placeholder="Enter Last Name"
+            error={errors.lastName}
           />
-          <Input
-            label="Username"
-            value={text}
-            onChangeText={text => onChangeText(text)}
-            placeholder="Enter Username"
-          />
+
           <Input
             label="Email"
-            value={text}
-            onChangeText={text => onChangeText(text)}
+            value={form.email}
+            onChangeText={value => {
+              onChange({name: 'email', value});
+            }}
             placeholder="Enter Email"
+            error={errors.email}
           />
 
           <Input
             label="Password"
-            value={text}
-            onChangeText={text => onChangeText(text)}
+            value={form.password}
+            onChangeText={value => {
+              onChange({name: 'password', value});
+            }}
             icon={<Text>Show</Text>}
             iconPosition="right"
             secureTextEntry
             placeholder="Enter Password"
+            error={errors.password}
           />
-          <CustomButton
-            onPress={e => console.log(e)}
-            loading
-            primary
-            title="Submit"
-          />
+          <CustomButton onPress={onSubmit} loading primary title="Submit" />
         </View>
         <View style={styles.createSection}>
           <Text style={styles.infoText}>Already have an Account?</Text>
--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,9 +1,40 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useState} from 'react';
 import {RegisterComponent} from '../../components/Signup';
+import {userSignUpFormValues} from '../../types/userSignUpFormValues';
 
 const SignUp = () => {
-  return <RegisterComponent />;
+  const [form, setForm] = useState<userSignUpFormValues>({});
+  const [errors, setErrors] = useState<userSignUpFormValues>({});
+
+  const onChange = ({name, value}: {name: string; value: string}) => {
+    setForm({...form, [name]: value});
+    if (value !== '') {
+      if (name === 'password' && value.length < 6) {
+        setErrors(prev => {
+          return {...prev, [name]: 'This field needs min 6 characters'};
+        });
+      } else {
+        setErrors(prev => {
+          return {...prev, [name]: null};
+        });
+      }
+    } else {
+      setErrors(prev => {
+        return {...prev, [name]: 'This field is required'};
+      });
+    }
+  };
+  const onSubmit = () => {
+    // validations
+  };
+  return (
+    <RegisterComponent
+      onChange={onChange}
+      onSubmit={onSubmit}
+      form={form}
+      errors={errors}
+    />
+  );
 };
 
 export {SignUp};
