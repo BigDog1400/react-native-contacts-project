446ded42cb1536da0b0912654893ebd4ed8a16bc
Isaac Peña <isaac.pr99@gmail.com>
2021-06-30 02:18:50 -0400

    fix: assign name to properties in object

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,11 +1,11 @@
 import {DEV_BACKEND_URL, PROD_BACKEND_URL} from 'react-native-dotenv';
 
 const devEnvironmentVariables = {
-  DEV_BACKEND_URL,
+  BACKEND_URL: DEV_BACKEND_URL,
 };
 
 const prodEnvironmentVariables = {
-  PROD_BACKEND_URL,
+  BACKEND_URL: PROD_BACKEND_URL,
 };
 
 export default __DEV__ ? devEnvironmentVariables : prodEnvironmentVariables;
