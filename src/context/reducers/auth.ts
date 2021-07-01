08f498402d94d81b4fd186f576748f51c3048bf4
Isaac Peña <isaac.pr99@gmail.com>
2021-07-01 03:43:28 -0400

    feat: add new cases to auth reducer

--- src/context/reducers/auth.ts
+++ src/context/reducers/auth.ts
@@ -4,6 +4,10 @@ import {authType} from '../initialStates/authState';
 export enum Types {
   LOGIN = 'LOGIN',
   SIGN_OUT = 'SIGN_OUT',
+  REGISTER_LOADING = 'REGISTER_LOADING',
+  REGISTER_SUCCESS = 'REGISTER_SUCCESS',
+  REGISTER_FAIL = 'REGISTER_FAIL',
+  CLEAR_AUTH_STATE = 'CLEAR_AUTH_STATE',
 }
 
 type AuthPayload = {
@@ -11,6 +15,10 @@ type AuthPayload = {
     email: string;
     password: string;
   };
+  [Types.REGISTER_LOADING]: undefined;
+  [Types.CLEAR_AUTH_STATE]: undefined;
+  [Types.REGISTER_SUCCESS]: object;
+  [Types.REGISTER_FAIL]: object;
   [Types.SIGN_OUT]: {};
 };
 
@@ -20,6 +28,14 @@ const auth = (state: authType, action: AuthActions) => {
   switch (action.type) {
     case Types.LOGIN:
       return state;
+    case Types.REGISTER_LOADING:
+      return {...state, loading: true};
+    case Types.REGISTER_SUCCESS:
+      return {...state, loading: false, data: action.payload};
+    case Types.REGISTER_FAIL:
+      return {...state, loading: false, error: action.payload};
+    case Types.CLEAR_AUTH_STATE:
+      return {...state, loading: false, data: null, error: null};
     default:
       return state;
   }
