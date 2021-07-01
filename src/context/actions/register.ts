504301b455ff1c97253cbd73d7b4b88224c971d2
Isaac Peña <isaac.pr99@gmail.com>
2021-07-01 03:42:11 -0400

    feat: create action types to handle register events

---
+++ src/context/actions/register.ts
@@ -0,0 +1,45 @@
+import {Dispatch} from 'react';
+import {userSignUpFormValues} from './../../types/userSignUpFormValues';
+import axiosInstance from '../../helpers/axiosInterceptor';
+import {AuthActions, Types} from '../reducers/auth';
+
+export const clearAuthState = () => (dispatch: Dispatch<AuthActions>) => {
+  dispatch({
+    type: Types.CLEAR_AUTH_STATE,
+  });
+};
+
+export default ({
+    email,
+    firstName: first_name,
+    lastName: last_name,
+    password,
+    username,
+  }: userSignUpFormValues) =>
+  (dispatch: Dispatch<AuthActions>) => {
+    dispatch({
+      type: Types.REGISTER_LOADING,
+    });
+    axiosInstance
+      .post('auth/register', {
+        password,
+        email,
+        first_name,
+        last_name,
+        username,
+      })
+      .then(res => {
+        dispatch({
+          type: Types.REGISTER_LOADING,
+          //   payload: res.data,
+        });
+      })
+      .catch(err => {
+        dispatch({
+          type: Types.REGISTER_FAIL,
+          payload: err.response
+            ? err.response.data
+            : {error: 'Something went wrong'},
+        });
+      });
+  };
