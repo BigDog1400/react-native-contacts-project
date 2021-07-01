d2fdd464e623ba958ea5cf83a7b2996a2145a42a
Isaac Peña <isaac.pr99@gmail.com>
2021-07-01 03:46:57 -0400

    feat: add register logic

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,11 +1,24 @@
-import React, {useState} from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import {useFocusEffect, useNavigation} from '@react-navigation/core';
+import React, {useCallback, useContext, useEffect, useState} from 'react';
 import {RegisterComponent} from '../../components/Signup';
+import {LOGIN} from '../../constants/routeNames';
+import register, {clearAuthState} from '../../context/actions/register';
+import {GlobalContext} from '../../context/Provider';
+import axiosInstance from '../../helpers/axiosInterceptor';
 import {userSignUpFormValues} from '../../types/userSignUpFormValues';
 
 const SignUp = () => {
   const [form, setForm] = useState<userSignUpFormValues>({});
   const [errors, setErrors] = useState<userSignUpFormValues>({});
-
+  useEffect(() => {
+    axiosInstance.get('/contacts').catch(err => console.log({err}));
+  }, []);
+  const {
+    state: {authState},
+    dispatch,
+  } = useContext(GlobalContext);
+  const {navigate} = useNavigation();
   const onChange = ({name, value}: {name: string; value: string}) => {
     setForm({...form, [name]: value});
     if (value !== '') {
@@ -26,13 +39,38 @@ const SignUp = () => {
   };
   const onSubmit = () => {
     // validations
+
+    if (
+      Object.values(form).length === 5 &&
+      Object.values(errors).every(item => !item) &&
+      Object.values(form).every(item => item && item?.trim().length > 0)
+    ) {
+      register(form)(dispatch);
+    }
   };
+  useEffect(() => {
+    if (authState.data) {
+      navigate(LOGIN);
+    }
+  }, [authState.data]);
+
+  useFocusEffect(
+    useCallback(() => {
+      return () => {
+        if (authState.data || authState.error) {
+          clearAuthState()(dispatch);
+        }
+      };
+    }, [authState.data, authState.error]),
+  );
   return (
     <RegisterComponent
       onChange={onChange}
       onSubmit={onSubmit}
       form={form}
       errors={errors}
+      error={authState.error}
+      loading={authState.loading}
     />
   );
 };
