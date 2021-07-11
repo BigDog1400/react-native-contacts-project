04dfd40b9433d4994d94fd1a012781ddfb47ce58
Isaac Peña <isaac.pr99@gmail.com>
2021-07-11 11:10:38 -0400

    feat: add headerLeft options

--- src/screens/Contacts/index.tsx
+++ src/screens/Contacts/index.tsx
@@ -1,8 +1,45 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+/* eslint-disable react-native/no-inline-styles */
+import {useNavigation} from '@react-navigation/native';
+import React, {useEffect} from 'react';
+import {Text, TouchableOpacity, View} from 'react-native';
 import {Container} from '../../components/common/Container';
+import {RouteProp, DrawerActions} from '@react-navigation/native';
+import {StackNavigationProp} from '@react-navigation/stack';
+import {HomeNavigatorStackParamList} from '../../navigations/HomeNavigator';
 
-const Contacts = () => {
+type ProfileScreenRouteProp = RouteProp<
+  HomeNavigatorStackParamList,
+  'Contacts'
+>;
+
+type ProfileScreenNavigationProp = StackNavigationProp<
+  HomeNavigatorStackParamList,
+  'Contacts'
+>;
+
+type Props = {
+  route: ProfileScreenRouteProp;
+  navigation: ProfileScreenNavigationProp;
+};
+
+const Contacts = ({route, navigation}: Props) => {
+  const {setOptions, dispatch} = useNavigation<ProfileScreenNavigationProp>();
+
+  useEffect(() => {
+    setOptions({
+      headerLeft: () => (
+        <TouchableOpacity
+          onPress={() => dispatch(DrawerActions.toggleDrawer())}>
+          <Text
+            style={{
+              padding: 10,
+            }}>
+            NAV
+          </Text>
+        </TouchableOpacity>
+      ),
+    });
+  }, []);
   return (
     <Container>
       <Text>Contacts Component</Text>
